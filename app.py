9dc915652f8b888309dc674bdeb53a3534f302ff
Ibrahim UX <vybezkhid7@gmail.com.com>
2025-07-26 19:32:16 +0000

    Update categories layout and dynamic category jobs view

--- app.py
+++ app.py
@@ -398,7 +398,8 @@ def toggle_dark_mode():
 
 # ------------------ Run ------------------
 
-if __name__ == '__main__':SS
-      with app.app_context():
+if __name__ == '__main__':
+    with app.app_context():
         db.create_all()
     app.run(debug=True)
+
